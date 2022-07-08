86336772c7b87873cfc895b7bac6eb04b03a8b17
Yeonseo-Jo <jys073190@gmail.com>
2022-07-08 18:20:26 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -16,3 +16,21 @@ while True:
 for i in range(p1):
   num+=1
   print("playerA :", num)
+  
+
+while True:
+  try:
+    p2 = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
+  except ValueError:
+    print("정수를 입력하세요: ")
+  else:
+    while True:
+      if p2 in range(1,4):
+        break
+      else:
+        p2 = int(input("1,2,3 중 하나를 선택하세요:"))
+    break
+
+for j in range(p2):
+  num+=1
+  print("playerB :", num)
