6439237ca5685839a1071df77b46230977ab6feb
Yeonseo-Jo <jys073190@gmail.com>
2022-07-08 18:18:37 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,14 @@
 num = 0
 
-p1 = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
+while True:
+  try:
+    p1 = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
+  except ValueError:
+    print("정수를 입력하세요: ")
+  else:
+    while True:
+      if p1 in range(1,4):
+        break
+      else:
+        p1 = int(input("1,2,3 중 하나를 선택하세요:"))
+    break
