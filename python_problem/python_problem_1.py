d53b9983bead0fc7c3762202c6cbb0b21f2378eb
Yeonseo-Jo <jys073190@gmail.com>
2022-07-08 19:14:54 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,44 +1,39 @@
-num = 0
+# 8단계
 
+
+
+def brGame():
+        while True:
+            try:
+                p=int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
+            except ValueError:
+                print("정수를 입력하세요: ")
+            else:
+                while True:
+                    if p in range(1,4):
+                        break
+                    else:
+                        p=int(input("1,2,3 중 하나를 선택하세요:" ))
+                return p
+            break
+
+
+
+num = 0
 while True:
-    while True:
-        try:
-            p1=int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
-        except ValueError:
-            print("정수를 입력하세요: ")
-        else:
-            while True:
-                if p1 in range(1,4):
-                    break
-                else:
-                    p1=int(input("1,2,3 중 하나를 선택하세요:" ))
-        break
-    
-    for i in range(p1):
+    player1 = brGame()
+    for i in range(player1):
         num += 1
         print("player A :", num)
         if num >= 31:
             print("playerB win!")
-            break 
+            break
         
     if num >= 31:
         break
-      
     
-    while True:
-        try:
-            p2=int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): "))
-        except ValueError:
-            print("정수를 입력하세요: ")
-        else:
-            while True:
-                if p2 in range(1,4):
-                    break
-                else:
-                    p2=int(input("1,2,3 중 하나를 선택하세요:" ))
-        break
-    
-    for j in range(p2):
+    player2 = brGame()
+    for j in range(player2):
         num+=1
         print("player B :", num)
         if num >= 31:
