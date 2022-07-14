b98ac98ab2f15e5f92f7df1a4bb51cd5ac3a5742
Yeonseo-Jo <jys073190@gmail.com>
2022-07-14 22:48:19 +0900

    update, delete 기능 구현

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -5,5 +5,8 @@ app_name = "reviews"
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('create', views.create, name="create")
-]
+    path('create', views.create, name="create"),
+    path('review/<int:id>', views.detail, name="detail"),
+    path('review/<int:id>/update', views.update, name="update"),
+    path('review/<int:id>/delete', views.delete, name="delete"),
+    ]
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -28,3 +28,37 @@ def create(request):
         return redirect("/")
     
     return render(request, template_name="reviews/create.html", context={})
+
+def detail(request, id):
+    review = Review.objects.get(id=id)
+    context = {
+        "review" : review
+    }
+    return render(request, template_name="reviews/detail.html", context=context)
+
+def update(request, id):
+    if request.method == "POST":
+        title = request.POST["title"]
+        r_year = request.POST["r_year"]
+        genre = request.POST["genre"]
+        grade = request.POST["grade"]
+        r_time = request.POST["r_time"]
+        r_content = request.POST["r_content"]
+        director = request.POST["director"]
+        actor = request.POST["actor"]
+        
+        review = Review.objects.filter(id=id).update(title=title, r_year=r_year, genre=genre, grade=grade, r_time=r_time, r_content=r_content, director=director, actor=actor)
+        
+        return redirect(f"/review/{id}")
+    
+    elif request.method =="GET":
+        review = Review.objects.get(id=id)
+        context = {
+            "review":review
+        }
+        return render(request, template_name="reviews/update.html", context=context)
+
+def delete(request, id):
+    review=Review.objects.filter(id=id)
+    review.delete()
+    return redirect("/")
