a463a7fa15c3b5135c46020a48b044200b42a489
Yeonseo-Jo <jys073190@gmail.com>
2022-07-20 18:57:57 +0900

    페이지 구현 완료

--- SWIDEA_SITE/ideas/urls.py
+++ SWIDEA_SITE/ideas/urls.py
@@ -7,5 +7,13 @@ from django.conf.urls.static import static
 app_name = "ideas"
 urlpatterns = [
     path('', views.idea_home, name="idea_home"),
-    path('create', views.idea_create, name="idea_create")
-]+static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path('create', views.idea_create, name="idea_create"),
+    path('idea/detail/<int:id>', views.idea_detail, name="idea_detail"),
+    path('idea/update/<int:id>', views.idea_update, name="idea_update"),
+    path('idea/delete/<int:id>', views.idea_delete, name="idea_delete"),
+    path('devtool', views.devtool_list, name="devtool_list"),
+    path('devtool/create', views.devtool_create, name="devtool_create"),
+    path('devtool/detail/<int:id>', views.devtool_detail, name="devtool_detail"),
+    path('devtool/update/<int:id>', views.devtool_update, name="devtool_update"),
+    path('devtool/delete/<int:id>', views.devtool_delete, name="devtool_delete")
+    ]+static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- SWIDEA_SITE/ideas/views.py
+++ SWIDEA_SITE/ideas/views.py
@@ -1,5 +1,4 @@
-from re import template
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from .models import Devtool, Idea
 
 def idea_home(request):
@@ -12,17 +11,104 @@ def idea_home(request):
 def idea_create(request):
     if request.method == "POST":
         title = request.POST["title"]
-        image = request.FILES["image"]
+        image = request.FILES.get("image",False)
         content = request.POST["content"]
         interest = request.POST["interest"]
-        selected_devtool = request.POST["devtool"]
+        devtool = Devtool.objects.get(name = request.POST["devtool"])
         
-        Idea.objects.create(title=title, image=image, content=content, interest=interest, devtool=selected_devtool)
+        Idea.objects.create(title=title, image=image, content=content, interest=interest, devtool=devtool)
         
         return redirect("/")
     
-    ideas = Idea.objects.all()
+    devtools=Devtool.objects.all()
     context = {
-        "ideas":ideas
+        "devtools":devtools,
     }
     return render(request, template_name="ideas/idea_create.html", context=context)
+
+def idea_detail(request, id):
+    idea = Idea.objects.get(id=id)
+    context={
+        "idea":idea,
+    }
+    return render(request, template_name="ideas/idea_detail.html", context=context)
+
+def idea_update(request,id):
+    if request.method == "POST":
+        title = request.POST["title"]
+        image = request.FILES.get("image", False)
+        content = request.POST["content"]
+        interest = request.POST["interest"]
+        selected_devtool = Devtool.objects.get(name = request.POST["devtool"])
+        devtool = selected_devtool
+        
+        Idea.objects.filter(id=id).update(title=title, image=image, content=content, interest=interest, devtool=devtool)
+        
+        return redirect(f"/idea/detail/{id}")
+    
+    elif request.method == "GET":
+        idea = Idea.objects.get(id=id)
+        devtools = Devtool.objects.all()
+        context ={
+            "idea":idea,
+            "devtools":devtools,
+        }
+        return render(request, template_name="ideas/idea_update.html", context=context)
+
+def idea_delete(request, id):
+    if request.method == "POST":
+        Idea.objects.filter(id=id).delete()
+        return redirect('/')
+    
+    
+#개발툴 페이지 view   
+def devtool_list(request):
+    devtools = Devtool.objects.all()
+    context={
+        "devtools": devtools,
+    }
+    return render(request, template_name="devtools/devtool_list.html", context=context)
+
+def devtool_create(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        type = request.POST["type"]
+        explain = request.POST["explain"]
+        
+        Devtool.objects.create(name=name, type=type, explain=explain)
+        
+        return redirect("/devtool")
+    
+    devtools = Devtool.objects.all()
+    context={
+        "devtools":devtools
+    }
+    return render(request, template_name="devtools/devtool_create.html", context=context)
+
+def devtool_detail(request,id):
+    devtool=Devtool.objects.get(id=id)
+    context={
+        "devtool":devtool,
+    }
+    return render(request, template_name="devtools/devtool_detail.html", context=context)
+
+def devtool_update(request,id):
+    if request.method == "POST":
+        name = request.POST["name"]
+        type = request.POST["type"]
+        explain = request.POST["explain"]
+        
+        Devtool.objects.filter(id=id).update(name=name, type=type, explain=explain)
+        
+        return redirect(f"/devtool/detail/{id}")
+    elif request.method == "GET":
+        devtool = Devtool.objects.get(id=id)
+        context={
+            "devtool":devtool
+        }
+        return render(request, template_name="devtools/devtool_update.html", context=context)
+
+def devtool_delete(request,id):
+    if request.method == "POST":
+        Devtool.objects.filter(id=id).delete()
+        return redirect("/devtool")
